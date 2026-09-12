57dd6a068eb41cc3c23c6e378236f47903cc3d05
Karthik_1219 <vallampatlakarthik2@gmail.com>
2026-09-12 16:45:12 +0530

    Reference existing GitHub Actions IAM user

--- setup/terraform/main.tf
+++ setup/terraform/main.tf
@@ -312,8 +312,8 @@ resource "aws_iam_role_policy_attachment" "codebuild" {
 ####################
 # Github Action role
 ####################
-resource "aws_iam_user" "github_action_user" {
-  name = "github-action-user"
+data "aws_iam_user" "github_action_user" {
+  user_name = "github-action-user"
 }
 
 
@@ -328,6 +328,6 @@ data "aws_iam_policy_document" "github_policy" {
 
 resource "aws_iam_user_policy" "github_action_user" {
   name   = "github-action-deployment-policy"
-  user   = aws_iam_user.github_action_user.name
+  user   = data.aws_iam_user.github_action_user.user_name
   policy = data.aws_iam_policy_document.github_policy.json
 }
